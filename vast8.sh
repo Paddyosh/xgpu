7a13ceae95dd1644eee64c9effd595f5d4b50f17
Paddyosh <135702366+Paddyosh@users.noreply.github.com>
2024-02-18 11:37:16 +1000

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x8283A94c88f4394aD434C7403331d881156667e7/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x0b44dEE28FA457317801c6c01D317D146B195857/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
